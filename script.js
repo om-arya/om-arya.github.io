5f213bdbd598bc9990e3f661dd55fe9f1f7f4fa4
om-arya <om.arya0577@gmail.com>
2024-05-26 12:45:22 -0400

    Default to dark mode

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Scroll reveal effect on page load
+ScrollReveal({
+    distance: '10rem',
+    duration: 1500,
+});
+
+ScrollReveal().reveal('.home-content', { origin: 'top' });
+ScrollReveal().reveal('.home-content h1', { origin: 'left'});
+
+// Type welcome message
+const typed = new Typed('.typed-text', {
+    strings: ['Welcome to my portfolio.'],
+    typeSpeed: 80,
+});
+
 // Highlight navbar links as they are scrolled on
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -18,40 +33,25 @@ window.onscroll = () => {
     });
 };
 
-// Scroll reveal effect on page load
-ScrollReveal({
-    distance: '10rem',
-    duration: 1500,
-});
-
-ScrollReveal().reveal('.home-content', { origin: 'top' });
-ScrollReveal().reveal('.home-content h1', { origin: 'left'});
-
-// Type welcome message
-const typed = new Typed('.typed-text', {
-    strings: ['Welcome to my portfolio.'],
-    typeSpeed: 80,
-});
-
 // Switch between light/dark mode
 const switchMode = () => {
     let root = document.querySelector(':root');
-    let currentBgColor = getComputedStyle(root).getPropertyValue('--bg-main-color').trim();
+    let currentTextColor = getComputedStyle(root).getPropertyValue('--text-main-color').trim();
     let modeToggleIcon = document.querySelector('#mode-toggle i');
 
-    if (currentBgColor === 'white') {
-        root.style.setProperty('--bg-main-color', '#0f0f0f');
-        root.style.setProperty('--bg-second-color', '#151516');
-        root.style.setProperty('--text-main-color', 'white');
-        root.style.setProperty('--text-second-color', '#05a0aa');
-        modeToggleIcon.classList.remove('bxs-sun');
-        modeToggleIcon.classList.add('bx-moon');
-    } else {
+    if (currentTextColor === 'white') {
         root.style.setProperty('--bg-main-color', 'white');
         root.style.setProperty('--bg-second-color', '#f9f9f9');
         root.style.setProperty('--text-main-color', 'black');
         root.style.setProperty('--text-second-color', '#fa5f55');
         modeToggleIcon.classList.remove('bx-moon');
         modeToggleIcon.classList.add('bxs-sun');
+    } else {
+        root.style.setProperty('--bg-main-color', '#0f0f0f');
+        root.style.setProperty('--bg-second-color', '#151516');
+        root.style.setProperty('--text-main-color', 'white');
+        root.style.setProperty('--text-second-color', '#05a0aa');
+        modeToggleIcon.classList.remove('bxs-sun');
+        modeToggleIcon.classList.add('bx-moon');
     }
 }
