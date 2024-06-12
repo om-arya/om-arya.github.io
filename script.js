101fad454c1deb8ac5257f934a823abb99b3cb30
om-arya <om.arya0577@gmail.com>
2024-06-11 21:38:41 -0400

    Start back content and styling

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-const flipCardButton = document.querySelector('.flip-card-button');
-flipCardButton.addEventListener('click', () => {
+const flipButtonFront = document.querySelector('.flip-button-front');
+const flipButtonBack = document.querySelector('.flip-button-back');
+
+flipButtonFront.addEventListener('click', () => {
     const front = document.querySelector('.front');
     const back = document.querySelector('.back');
 
-    if (front.style.visibility = "visible") {
-        front.classList.toggle('visible');
-        front.classList.toggle('hidden');
-        back.classList.toggle('hidden');
-        back.classList.toggle('visible');
-    } else {
-        back.classList.toggle('visible');
-        back.classList.toggle('hidden');
-        front.classList.toggle('hidden');
-        front.classList.toggle('visible');
-    }
+    front.classList.toggle('visible');
+    front.classList.toggle('hidden');
+    back.classList.toggle('hidden');
+    back.classList.toggle('visible');
+});
+
+flipButtonBack.addEventListener('click', () => {
+    const front = document.querySelector('.front');
+    const back = document.querySelector('.back');
+
+    back.classList.toggle('visible');
+    back.classList.toggle('hidden');
+    front.classList.toggle('hidden');
+    front.classList.toggle('visible');
 });
